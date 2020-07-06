5989c275080121e3b712662397d26f57d2dbf236
LivesayMe <48233071+LivesayMe@users.noreply.github.com>
2020-07-06 09:34:02 -0600

    Removed navigation drawer

--- app/src/main/java/com/example/cs246/RestaurantDisplay.java
+++ app/src/main/java/com/example/cs246/RestaurantDisplay.java
@@ -93,7 +93,7 @@ public class RestaurantDisplay extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.restaurant_display, menu);
+
         return true;
     }
 }
